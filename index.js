a32b272a1d97e57d66fffcda9d03b170a3b50de7
ismailnarin <ismailnrn94@gmail.com>
2023-12-17 15:33:21 +0300

    slider

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("deneme");
+document.addEventListener("DOMContentLoaded", function () {
+  const sliderPhoto = Array.from(
+    { length: 25 },
+    (_, index) => `assets/image/slider/${index + 1}.jpg`
+  );
+  console.log(sliderPhoto);
+  const sliderNumberDiv = document.getElementById("sliderNumber");
+  const sliderImage = document.getElementById("sliderImage");
+  for (let i = 1; i <= sliderPhoto.length; i++) {
+    const spanElement = document.createElement("span");
+    spanElement.textContent = i + " ";
+    spanElement.addEventListener("mouseover", function () {
+      console.log(sliderPhoto[i - 1]);
+      sliderImage.src = sliderPhoto[i - 1];
+    });
+    sliderNumberDiv.appendChild(spanElement);
+  }
+});
